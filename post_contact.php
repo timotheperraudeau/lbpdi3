73bbfa5a41e68360a5d4518767b4f93bf82fd5e9
Timothé Perraudeau <timotheeperraudeau@hotmail.fr>
2021-12-22 14:43:01 +0100

    mise en place du captcha et formatage des mails

--- post_contact.php
+++ post_contact.php
@@ -1,22 +1,9 @@
 <?php
 $errors = [];
-$url = "http://www.google.com/recaptcha/api/siteverify?secret=6LdLNLwdAAAAAEs4Me7RCS59rn64Wa54fqs_RaH6&response={$_POST['recaptcha-response']}";
 
 if(empty($_POST['recaptcha-response'])){
-    $errors['captcha'] = "erreur de validation formulaire. Merci de rééssayer";
-}else{
-$response = file_get_contents($url);
+    $errors['captcha'] = "Problème technique. Merci de rééssayer";
 }
-
-if(empty($response) || is_null(($response))){
-    $errors['response'] = "Problème technique. Veuillez recommencer";
-}
-$data = json_decode($response);
-
-if($data['success'] = false){
-    $errors['response'] = "Problème technique. Veuillez recommencer";
-}
-
 
 if(!array_key_exists('name', $_POST)||$_POST['name'] == ''){
     $errors['name'] = "Veuillez renseigner votre nom";
@@ -40,13 +27,10 @@ $_SESSION['input'] = $_POST;
 header('location: contact.php');
 }else{
 $_SESSION['Success'] = 1;
-$name = $_POST['name'];
-$surname = $_POST['surname'];
-$email = $_POST['email'];
-$tel = $_POST['tel'];
 $message = $_POST['message'];
-$headers = 'FROM: https//www.lbpdi.fr ';
-@mail('contact@lbpdi.fr', 'Formulaire de contact',$name, $surname, $email, $tel, $message, $headers);  
+$headers = 'FROM: https//www.lbpdi.fr';
+@mail("timotheeperraudeau@hotmail.fr", 'Formulaire de contact lbpdi.fr', "Nom: " . $_POST['name'] . "\n" . "Prénom: " . $_POST['surname'] . "\n" 
+. "Mail: " . $_POST['email'] . "\n" . "Téléphone: " . $_POST['tel'] . "\n" . "Message: " . $message, $headers);  
 header('location: contact.php');
 }
 
